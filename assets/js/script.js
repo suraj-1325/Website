f0fae467b594e59cc0da251d487f3b946bae8e91
suraj-1325 <surajtamang1308@gmail.com>
2025-10-21 18:48:00 +0545

    web

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Cyber Security", "Information Security", "Information Technology"],
+    strings: ["Security Analyst", "SOC Analyst/Engineer", "Information Technology"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
