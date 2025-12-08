da373dbaadddc264cf264890e264179df983deff
suraj-1325 <surajtamang1308@gmail.com>
2025-12-08 16:45:24 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["SOC Analyst/Engineer", "Information Technology", "Malware Researcher"],
+    strings: ["SOC Analyst/Engineer", "Information Technology"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
